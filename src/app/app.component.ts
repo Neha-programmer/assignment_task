adac313c35e9d2295f0c88ebc6cda49572ddd4a5
Neha-programmer <neha.goel.sml@gmail.com>
2023-05-03 00:58:20 +0530

    task ui with its functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// defining the interface for the table elements 
+export interface tableElements {
+  firstName: string;
+  lastName: number;
+  chartNo: number;
+  address1: string;
+}
+
+const TABLE_DATA: tableElements[] = [];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'assignment_task';
+
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['firstName', 'lastName', 'chartNo', 'address1'];
+  dataSource = TABLE_DATA;
+  searchByTextValue:string='';
+  firstNameValue:string='';
+  lastNameValue:string='';
+  chartNoValue:string='';
+  address1Value:string=''
+
+  constructor(){}
+
+  ngOnInit() {}
+
+  //searchBy function for searchBy card on the left of the UI 
+  searchBy(event:any,searchValueType:string){
+    console.log('here',event.target.value);
+    if(searchValueType=='First Name'){
+      this.searchByTextValue="\\" + event.target.value;
+    }
+    else if(searchValueType=='Last Name'){
+      this.searchByTextValue=this.searchByTextValue + "@" + event.target.value;
+    }
+    else if(searchValueType=='Chart no'){
+      this.searchByTextValue=this.searchByTextValue + '#' + event.target.value;
+    }
+    else{
+      this.searchByTextValue=this.searchByTextValue + '$' + event.target.value;
+    }
+  }
+
+  //search function for the serach card on the right of the UI 
+  search(event:any){
+    console.log('here-2',event.target.value);
+  }
+
+  // clear function in-order to clear the values whatever entered in the input field
+  clearSearchInput(){
+    this.searchByTextValue='';
+    this.firstNameValue='';
+    this.lastNameValue='';
+    this.chartNoValue='';
+    this.address1Value='';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatTableModule,
+    NgbModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
